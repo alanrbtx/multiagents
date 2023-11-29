7080b04f2fb0b109f84f7b17e5c0413e3a3193d4
a.gazzaev <a.gazzaev@mts.ai>
2023-11-29 15:33:38 +0300

    agents

--- agents/conversation.py
+++ agents/conversation.py
@@ -5,26 +5,26 @@ class TwoAgentsCoversation:
             self,
             agent1: LLMAgent,
             agent2: LLMAgent,
-            max_new_messages: int = 4
+            max_rounds: int = 2
     ):
         
         self.agent1 = agent1
         self.agent2 = agent2
-        self.max_new_messages = max_new_messages
+        self.max_rounds = max_rounds
         self.context = list()
     
     
     def start_conversation(self, user_prompt):
         system_prompt = (
             "Чат между двумя агентами"
-            "Вы должны решить одну задачу"
             "ENGINEER: ты генерируешь код шаг за щагом"
-            "CRITIC: ты определяешь насколько правильный сгенерирован код"
-            "Если вы считаете, что задача решена сгенерируйте SOLVED"
+            "CRITIC: ты определяешь насколько правильно сгенерирован код"
+            "CRITIC, если ты считаешь, что задача решена правильно сгенерируй SOLVED"
+            "Задача: "
         )
 
         self.context.append(system_prompt + user_prompt)
-        for i in range(self.max_new_messages + 1):
+        for i in range(self.max_rounds):
             joined_context = "\n".join(self.context)
             agent1_responce = self.agent1.sample(joined_context + "\n" + "CRITIC:")
             self.context.append(agent1_responce)
